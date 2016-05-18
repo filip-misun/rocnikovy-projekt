7fd63c76a0fc79520d60354c9a3020e3ae460984
dodo42 <j.rajnik@gmail.com>
2016-05-18 23:12:49 +0200

    Add print and read from Scanner

--- src/rocnikovyprojekt/CFGrammar.java
+++ src/rocnikovyprojekt/CFGrammar.java
@@ -5,10 +5,14 @@
  */
 package rocnikovyprojekt;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
 import java.util.Set;
 
 /**
@@ -33,6 +37,39 @@ public class CFGrammar {
         startSymbol = s;
     }
     
+    /**
+     * Reads and initializes grammar from Scanner.
+     * The input format is as following:
+     * - first line contains start symbol
+     * - each next lines contains rules.
+     * @param s Scanner which is read from
+     */
+    public CFGrammar(Scanner s){
+        String line = null;
+        while(s.hasNext()){
+            line = s.nextLine();
+            if(line.length() < 2 || !line.substring(0,2).equals("//")){
+                break;
+            }
+        }
+        startSymbol = line;
+        while(s.hasNext()){
+            line = s.nextLine();
+            String[] args = line.split(" -> ");
+            Object nonterm = args[0];
+            for(String word : args[1].split(" | ")){
+                rules.add(new Rule(nonterm, new Word(Arrays.asList(word.split(" ")))));
+            }
+        }
+    }
+    
+    public void print(PrintStream out){
+        out.println(startSymbol);
+        for(Rule r : rules){
+            out.println(r.nonterminal + " -> " + r.word);
+        }
+    }
+    
     public class Rule {
         Object nonterminal;
         Word word;
@@ -40,6 +77,20 @@ public class CFGrammar {
         public Rule(Object n, Word w){
             nonterminal = n;
             word = w;
+        }
+        
+        @Override
+        public boolean equals(Object o){
+            if(!(o instanceof Rule)){
+                return false;
+            }
+            Rule r = (Rule) o;
+            return this.nonterminal == r.nonterminal && this.word == r.word;
+        }
+        
+        @Override
+        public int hashCode(){
+            return Objects.hash(nonterminal, word);
         }
     }
     

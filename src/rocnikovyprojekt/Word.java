8234660dccbba46bd8a6751d4f45ee255825d32f
dodo42 <j.rajnik@gmail.com>
2016-06-17 20:18:37 +0200

    Add comments

--- src/rocnikovyprojekt/Word.java
+++ src/rocnikovyprojekt/Word.java
@@ -6,46 +6,82 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Class representing words in formal languages.
+ * A word can conaints of any objects as symbols. This class is build persistent,
+ * so all methods called on a word does not modify it, but return new word.
+ */
 public class Word {
 	
+    /**
+     * Static field for empty word -- epsilon.
+     */
 	public final static Word EPSILON = new Word(){
             @Override
             public String toString(){
                 return "epsilon";
             }
         };
+        /** List of symbols in word */
 	private List<Object> symbols = new ArrayList<>();
 	
+        /**
+         * Initializes empty word.
+         */
 	public Word() {
 		super();
 	}
 	
+        /**
+         * Initializes new word consisting of symbols in given Collection.
+         */
 	public Word(Collection<Object> symbols) {
 		this.symbols = new ArrayList<>(symbols);
 	}
         
+        /**
+         * Initializes new word consisting of symbols in given array of Objects.
+         * @param array 
+         */
         public Word(Object[] array){
             symbols.addAll(Arrays.asList(array));
         }
         
+        /**
+         * Initializes new word consisting of characters in given string. 
+         */
         public Word(String s){
             for(int i = 0; i < s.length(); i++){
                 symbols.add((new Character(s.charAt(i))).toString());
             }
         }
         
+        /**
+         * Initializes word as a copy of another word.
+         */
         public Word(Word word){
             this.symbols = new ArrayList<>(word.symbols);
         }
         
+        /**
+         * Initializes word consisting of one symbol.
+         * @param ch 
+         */
         public Word(Object ch){
             this.symbols.add(ch);
         }
 	
+        /**
+         * Returns symbol in word at given position. 
+         */
 	public Object symbolAt(int index) {
 		return symbols.get(index);
 	}
 	
+        /**
+         * Append given symbol to the end of this word and returns modified
+         * word.
+         */
 	public Word append(Object symbol) {
             Word w = new Word();
             w.symbols = new ArrayList<>(symbols);
@@ -53,6 +89,10 @@ public class Word {
             return w;
 	}
         
+        /**
+         * Append given word to the end of this word and returns modified
+         * word.
+         */
         public Word append(Word word){
             Word w = new Word();
             w.symbols = new ArrayList<>(symbols);
@@ -60,24 +100,42 @@ public class Word {
             return w;
         }
 	
+        /**
+         * Returns length of this word.
+         */
 	public int length() {
 		return symbols.size();
 	}
         
+        /**
+         * Returns whether this word is empty.
+         */
         public boolean isEmpty(){
             return symbols.isEmpty();
         }
         
+        /**
+         * Returns last symbol of this word.
+         */
         public Object last(){
             return symbols.get(symbols.size() - 1);
         }
         
+        /**
+         * Returns word without last symbol.
+         */
         public Word pop(){
+            if(this.length() == 0){
+                return this;
+            }
             Word w = new Word(symbols);
             w.symbols.remove(w.symbols.size() - 1);
             return w;
         }
         
+        /**
+         * Returns ArrayList of symbols in this word.
+         */
         public ArrayList<Object> getSymbols(){
             return new ArrayList<>(symbols);
         }
@@ -97,6 +155,9 @@ public class Word {
             return new Word(newWord);
         }
         
+        /**
+         * Returns reverse of this word.
+         */
         public Word reverse(){
             return new Word(Lists.reverse(symbols));
         }
